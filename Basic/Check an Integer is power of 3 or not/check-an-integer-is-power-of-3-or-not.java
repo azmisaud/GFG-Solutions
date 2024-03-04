170482ec61d9de2ec03a2cd1c2cc48cff0d1d134
Mohd Saud <157240150+azmisaud@users.noreply.github.com>
2024-03-04 16:39:28 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Check an Integer is power of 3 or not/check-an-integer-is-power-of-3-or-not.java
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.isPowerof3(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static String isPowerof3(int N){
+        // code here
+        if(1162261467%N==0) return "Yes";
+        else return "No";
+    }
+}
