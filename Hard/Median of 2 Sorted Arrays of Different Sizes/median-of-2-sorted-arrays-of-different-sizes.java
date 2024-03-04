81034731acda726aa84a3345e5c26aeab106d9f6
Mohd Saud <157240150+azmisaud@users.noreply.github.com>
2024-03-04 16:25:08 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Median of 2 Sorted Arrays of Different Sizes/median-of-2-sorted-arrays-of-different-sizes.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Driver
+{
+    public static void main(String args[]) 
+	{ 
+	    Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int []a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+            
+            int  m= sc.nextInt();
+            int []b = new int[m];
+            for (int i = 0; i < m; i++) b[i] = sc.nextInt();
+            
+            double res = new GFG().medianOfArrays(n, m, a, b);
+            
+            if (res == (int)res) System.out.println ((int)res);
+            else System.out.println (res);
+        }
+    		
+	} 
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class GFG 
+{ 
+    static double medianOfArrays(int n, int m, int a[], int b[]) 
+    {
+        // Your Code Here
+        int[] arr3=new int[n+m];
+        System.arraycopy(a,0,arr3,0,n);
+        System.arraycopy(b,0,arr3,n,m);
+        Arrays.sort(arr3);
+        if(!(arr3.length%2==0)) return arr3[arr3.length/2];
+        else return (double) (arr3[(arr3.length-1)/2]+arr3[arr3.length/2])/2;
+    }
+}
