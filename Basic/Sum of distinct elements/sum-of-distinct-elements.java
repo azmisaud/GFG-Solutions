605130578618996dfa2f6e4fae50ec0c53217792
Mohd Saud <157240150+azmisaud@users.noreply.github.com>
2024-03-21 21:17:35 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Sum of distinct elements/sum-of-distinct-elements.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().findSum(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    int findSum(int arr[], int n) {
+        // code here
+        HashSet<Integer> hs=new HashSet<>();
+        for(int x : arr) hs.add(x);
+        int sum=0;
+        for(int x : hs) sum+=x;
+        return sum;
+    }
+}
