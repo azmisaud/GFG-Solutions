dba7647e797a4d9cf05bf348033c2db41ecc908f
Mohd Saud <157240150+azmisaud@users.noreply.github.com>
2024-03-25 23:54:26 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Sum of numbers in string/sum-of-numbers-in-string.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) throws IOException{
+	    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st=new StringTokenizer(br.readLine());
+	    int t=Integer.parseInt(st.nextToken());
+	    while(t-->0){
+	        String s=br.readLine();
+	        
+	        Solution obj = new Solution();
+	        
+	        System.out.println(obj.findSum(s));
+	    }
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    //Function to calculate sum of all numbers present in a string.
+    public static long findSum(String str)
+    {
+        // your code here
+        String[] str2=str.split("\\s*[a-zA-Z]+\\s*");
+        long sum=0;
+        for(int i=0;i<str2.length;i++){
+            if(str2[i].matches("[0-9]+")){
+            long n=Long.parseLong(str2[i]);
+            sum+=n;
+            }
+        }
+        return sum;
+    }
+    
+}
